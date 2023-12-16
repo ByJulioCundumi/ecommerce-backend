b8116e5a1e7b2fb263854b8eb371c2134bcef2cf
ByJulioCundumi <julio.ce.dev@gmail.com>
2023-12-16 09:17:50 -0500

    db conection working and user entity added

--- src/config/config.ts
+++ src/config/config.ts
@@ -3,7 +3,7 @@ import {config} from "dotenv";
 config();
 export const PORT = parseInt(process.env.PORT as string) | 3000;
 export const HOST = process.env.HOST as string | "localhost";
-export const DB_PORT = parseInt(process.env.DB_PORT as string) | 3000;
+export const DB_PORT = parseInt(process.env.DB_PORT as string) | 3306;
 export const DB_HOST = process.env.DB_HOST as string | "localhost";
 export const DB_USERNAME = process.env.DB_USERNAME as string | "root";
 export const DB_PASSWORD = process.env.DB_PASSWORD as string | "mysql8";
---
+++ src/entity/User.ts
@@ -0,0 +1,27 @@
+import { Entity, BaseEntity, Column, PrimaryGeneratedColumn, CreateDateColumn, UpdateDateColumn } from "typeorm";
+
+@Entity()
+export class User extends BaseEntity {
+
+    @PrimaryGeneratedColumn()
+    id:number;
+
+    @Column({nullable: false})
+    firstname:string;
+
+    @Column({nullable: false})
+    lastname:string;
+
+    @Column({unique: true, nullable: false})
+    email:string;
+
+    @Column({nullable: false})
+    password:string;
+
+    @CreateDateColumn()
+    createdAt:Date;
+
+    @UpdateDateColumn()
+    updatedAt:Date;
+
+}
